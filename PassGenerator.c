150c1563ffff705136852885eaaa4e516a807915
Complllex <minor6255@gmail.com>
2021-07-15 16:05:06 +0300

    Update 1.5

--- PassGenerator.c
+++ PassGenerator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
     int count;
@@ -8,14 +9,15 @@ int main(){
     char* alphabet[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z","0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "'", "`", "\"", "!", "?", ".", ",", ":", ";", "$", "%", "@", "~", "#", "(", ")", "<", ">", "{", "}", "[", "]", "_", "*", "-", "+", "^", "=", "/", "|", "\\"};
     printf("Password generator v1 by Complllex\n");
     printf("How many letters should your password contain?\n");
-    scanf("%d", &count);
+    scanf("%ld", &count);
     if(count >= 50){
     count = 50;
     }
     printf("Your password: ");
+    srand(time(0));
     while (chech++ != count)
     {
-       letter = rand()%93;
+       letter = rand() % 93;
        printf("%s", alphabet[letter]);
     }
     printf("\n");
