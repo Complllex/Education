031f657d7367b124f0841c832f4274ed43084713
complllex <minor6255@gmail.com>
2021-10-20 12:18:55 +0300

    Update

--- strstr_tests/as_strstr.c
+++ strstr_tests/as_strstr.c
@@ -6,6 +6,7 @@ char as_strstr(char const* text, char const* find)
 {
   int result = 0;
   int result2 = 0;
+  char const* findforreturn = find;
   g_assert(text != NULL);
   g_assert(find != NULL);
   while (*find != '\0') {
@@ -17,6 +18,7 @@ char as_strstr(char const* text, char const* find)
     int numbertext = *text - 96;
     if (numbertext == -64) {
       if (result2 == result) {
+        printf("return: %s%s\n", findforreturn, text);
         return 1;
       } else {
         result2 = 0;
@@ -29,6 +31,7 @@ char as_strstr(char const* text, char const* find)
   }
   if (*text == '\0') {
     if (result2 == result) {
+      printf("return: %s\n", findforreturn);
       return 1;
     }
   }
